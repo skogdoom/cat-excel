0d3bfe43e9e1a9d955bd30f3b5a7cc7e08d27cd6
David Andreasson <647640+skogdoom@users.noreply.github.com>
2020-10-28 22:01:25 +0100

    create a new excel file with the result

--- excel-cat.py
+++ excel-cat.py
@@ -9,13 +9,21 @@ if __name__ == '__main__':
     wb1 = xlrd.open_workbook(filename='excel_files/a.xlsx')
     wb2 = xlrd.open_workbook(filename='excel_files/b.xlsx')
 
+    new_wb = xlwt.Workbook()
+
     for sheet_idx in range(MAX_SHEETS):
         sheet1 = wb1.sheet_by_index(sheet_idx)
         sheet2 = wb2.sheet_by_index(sheet_idx)
+
+        cat_sheet = new_wb.add_sheet(sheet1.name)
 
         for col_idx in range(MAX_COLS):
             values_1 = sheet1.col_values(col_idx, 0, MAX_ROWS)
             values_2 = sheet2.col_values(col_idx, 0, MAX_ROWS)
 
             cat_col = values_1 + values_2
-            print(cat_col)
+
+            for row_idx, value in enumerate(cat_col):
+                cat_sheet.write(row_idx, col_idx, value)
+
+    new_wb.save('excel_files/result.xls')

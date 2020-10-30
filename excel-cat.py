5f16010071072e41d30e75e06991bd684790cf5e
David Andreasson <647640+skogdoom@users.noreply.github.com>
2020-10-30 18:34:15 +0100

    concatenate 2 excel files

--- excel-cat.py
+++ excel-cat.py
@@ -1,25 +1,21 @@
 import xlrd
 import xlwt
 
-MAX_SHEETS = 1
-MAX_COLS = 1
-MAX_ROWS = 10
-
 if __name__ == '__main__':
     wb1 = xlrd.open_workbook(filename='excel_files/a.xlsx')
     wb2 = xlrd.open_workbook(filename='excel_files/b.xlsx')
 
     new_wb = xlwt.Workbook()
 
-    for sheet_idx in range(MAX_SHEETS):
+    for sheet_idx in range(wb1.nsheets):
         sheet1 = wb1.sheet_by_index(sheet_idx)
         sheet2 = wb2.sheet_by_index(sheet_idx)
 
         cat_sheet = new_wb.add_sheet(sheet1.name)
 
-        for col_idx in range(MAX_COLS):
-            values_1 = sheet1.col_values(col_idx, 0, MAX_ROWS)
-            values_2 = sheet2.col_values(col_idx, 0, MAX_ROWS)
+        for col_idx in range(sheet1.ncols):
+            values_1 = sheet1.col_values(col_idx, 0, sheet1.nrows)
+            values_2 = sheet2.col_values(col_idx, 0, sheet2.nrows)
 
             cat_col = values_1 + values_2
 
